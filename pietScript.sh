bff172207eabcec50ac0f7bc0b7a1a10f91adfd2
R2 <lethe@R2.local>
2018-12-01 15:33:51 -0600

    finalish

---
+++ pietScript.sh
@@ -0,0 +1,17 @@
+PROJECT="VRDGHack-A-Thon!"
+GROUPNAME="The Colorful Bunch"
+
+echo "****************************************"
+echo "*            "$PROJECT"          *"
+echo "****************************************"
+echo
+echo $GROUPNAME
+echo
+
+open ./nhello.ppm
+
+npiet -tpic nhello.ppm
+
+
+echo
+echo
